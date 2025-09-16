b603d61f1b3a6bc90589e37246b84dda5b0b331a
Erik Rasmussen <erik@attio.com>
2025-09-16 09:11:18 +0200

    Upgrade to use Record Combobox

--- src/components/comboboxes/companies-combobox.tsx
+++ src/components/comboboxes/companies-combobox.tsx
@@ -1,31 +1,21 @@
 import type {FormApi} from "attio/client"
 import type {LogCustomerRequestFormSchema} from "../log-customer-request-dialog"
-import {companiesProvider} from "./companies-options-provider"
 
 export function CompaniesCombobox({
     Combobox,
     companyId,
 }: {
-    Combobox: FormApi<LogCustomerRequestFormSchema>["Combobox"]
+    Combobox: FormApi<LogCustomerRequestFormSchema>["Experimental_RecordCombobox"]
     companyId?: string
 }) {
     if (companyId) {
-        return (
-            <Combobox
-                label="Company"
-                name="companyRecordId"
-                options={companiesProvider}
-                __experimental_decorated
-                disabled
-            />
-        )
+        return <Combobox label="Company" name="companyRecordId" recordTypes="company" disabled />
     }
     return (
         <Combobox
             label="Company"
             name="companyRecordId"
-            __experimental_decorated
-            options={companiesProvider}
+            recordTypes="company"
             placeholder="Select a company..."
             searchPlaceholder="Search companies..."
         />
