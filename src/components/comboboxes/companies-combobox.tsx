43f81ed0dc3b04160b1d491b58bc065763d0facc
Kasper F. Kristensen <kasper.kristensen@attio.com>
2025-10-01 14:51:58 +0200

    fix: lint

--- src/components/comboboxes/companies-combobox.tsx
+++ src/components/comboboxes/companies-combobox.tsx
@@ -1,30 +1,30 @@
-import type { FormApi } from "attio/client";
-import type { LogCustomerRequestFormSchema } from "../log-customer-request-dialog";
+import type {FormApi} from "attio/client"
+import type {LogCustomerRequestFormSchema} from "../log-customer-request-dialog"
 
 export function CompaniesCombobox({
-  Combobox,
-  companyId,
+    Combobox,
+    companyId,
 }: {
-  Combobox: FormApi<LogCustomerRequestFormSchema>["Experimental_AttioRecordCombobox"];
-  companyId?: string;
+    Combobox: FormApi<LogCustomerRequestFormSchema>["Experimental_AttioRecordCombobox"]
+    companyId?: string
 }) {
-  if (companyId) {
+    if (companyId) {
+        return (
+            <Combobox
+                label="Company"
+                name="companyRecord"
+                objectSlug="companies"
+                disabled
+            />
+        )
+    }
     return (
-      <Combobox
-        label="Company"
-        name="companyRecord"
-        objectSlug="companies"
-        disabled
-      />
-    );
-  }
-  return (
-    <Combobox
-      label="Company"
-      name="companyRecord"
-      objectSlug="companies"
-      placeholder="Select a company..."
-      searchPlaceholder="Search companies..."
-    />
-  );
+        <Combobox
+            label="Company"
+            name="companyRecord"
+            objectSlug="companies"
+            placeholder="Select a company..."
+            searchPlaceholder="Search companies..."
+        />
+    )
 }
